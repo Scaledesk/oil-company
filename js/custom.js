2523cc647a8d057cc286349fb59d28d150d1c74c
smriti <iamsmritid@gmail.com>
2016-11-04 18:17:02 +0530

    2 sections added

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
 /**
  * Created by smriti on 22/10/16.
  */
+
+//owl carousel
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+
+    });
+
+});
